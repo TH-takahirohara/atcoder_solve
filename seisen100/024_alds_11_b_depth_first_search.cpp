86c6079f4443703c5e812bc3b0d43a2c94c90769
Takahiro Hara <takahiro.hb@gmail.com>
2022-03-12 02:22:04 +0900

    update seisen100

---
+++ seisen100/024_alds_11_b_depth_first_search.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define fore(i,a) for(auto &i:a)
+
+int T = 0;
+vector<int> F;
+vector<int> C;
+vector<bool> seen;
+
+void dfs(vector<vector<int> > &G, int x) {
+  seen.at(x) = true;
+  T++;
+  F.at(x) = T;
+  
+  for (int nx : G.at(x)) {
+    if (seen.at(nx)) continue;
+    dfs(G, nx);
+  }
+  
+  T++;
+  C.at(x) = T;
+}
+
+int main() {
+  int N;
+  cin >> N;
+  vector<vector<int> > G(N+1);
+  
+  seen.assign(N+1, false);
+  F.assign(N+1, 0);
+  C.assign(N+1, 0);
+  
+  rep(i,0,N) {
+    int U, K;
+    cin >> U >> K;
+    rep(j,0,K) {
+      int tmp;
+      cin >> tmp;
+      G.at(U).push_back(tmp);
+    }
+  }
+  
+  rep(i,1,N+1) {
+    if (seen.at(i)) continue;
+    dfs(G, i);
+  }
+  
+  rep(i,1,N+1) {
+    cout << i << " " << F.at(i) << " " << C.at(i) << endl;
+  }
+}
+
