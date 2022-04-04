fcf52f06b8496a76d149bfe66928cb433448ed3d
Takahiro Hara <takahiro.hb@gmail.com>
2022-04-05 01:40:19 +0900

    add 072 seisen100

---
+++ seisen100/072_abc034_c_keiro.cpp
@@ -0,0 +1,42 @@
+// 前処理のテーブルの配列長に注意。（本問題ではW+Hより長い配列長にする必要がある。）
+// 最初、配列長（MAXの値）を110000として、REを出してしまった。
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define fore(i,a) for(auto &i:a)
+
+const int MAX = 1000000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+int main() {
+  COMinit();
+  
+  int W,H;
+  cin >> W >> H;
+  W--;H--;
+  
+  cout << COM(W+H, W) << endl;
+}
