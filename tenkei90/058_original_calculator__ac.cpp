d058e90e85a6eb2498ef8ae2976e8c07982378c5
Takahiro Hara <takahiro.hb@gmail.com>
2022-05-18 01:25:48 +0900

    add 058(AC) tenkei90

--- tenkei90/058_original_calculator__ac.cpp
+++ tenkei90/058_original_calculator__ac.cpp
@@ -1,6 +1,11 @@
 // 058 キーワード：周期性を考える
-// 方向性は合っていると思うが、どうしても3つWAが出る。テストケースを見ても分からず。
-// -> その後解決した。acの方に書いている。
+// waの解法では、loop_start_cnt をloop開始の箇所から-1していたが、これがいけなかった。
+// （0 -> 1 -> 2 -> 3 -> 1 -> 2 -> 3 -> ... というループを、 0 -> [ 1 -> 2 -> 3 ] -> [ 1 -> 2 -> 3 ] -> ... という周期で捉えていた。）
+// このようにしていたことで、例えば 0 -> 1 -> 2 -> 3 -> 1 -> 2 -> 3 -> ... というループだったとしても、
+// loop_start_cntが0になってしまうので、 0 -> 1 -> 2 -> 3 -> 0 -> 1 -> 2 -> 3 -> ... というループとして取り扱ってしまっていたため、WAとなっていた。
+// loop_start_cntは-1せず、上記のループで言えば、
+// 0 -> [ 1 -> 2 -> 3 ] -> [ 1 -> 2 -> 3 ] -> ... ではなく
+// 0 -> 1 -> [ 2 -> 3 -> 1 ]　-> [ 2 -> 3 -> 1 ] -> ... として捉え直して書き直すことで、WAしていた箇所がACになった。
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -35,20 +40,22 @@ int main() {
   
   ll now = N;
   ll loop = 0;
+  ll lsi = -1;
   ll loop_start_cnt = 0;
-  rep(i,0,1000000) {
+  rep(i,0,100000) {
     ll nx = next_num(now);
     if (vec.at(nx) == -1) {
       vec.at(nx) = vec.at(now) + 1;
       now = nx;
     } else {
       loop = vec.at(now) - vec.at(nx) + 1;
-      loop_start_cnt = vec.at(nx) - 1;
+      lsi = nx;
+      loop_start_cnt = vec.at(nx);
       break;
     }
   }
   
-  if (K <= loop_start_cnt) {
+  if (K < loop_start_cnt) {
     ll ind = N;
     rep(i,0,K) {
       ll nx = next_num(ind);
@@ -57,7 +64,7 @@ int main() {
     cout << ind << endl;
     return 0;
   } else {
-    ll push_cnt = (K - loop_start_cnt) % loop + loop_start_cnt;
+    ll push_cnt = (K - (loop_start_cnt + 1)) % loop + loop_start_cnt + 1;
     ll ind = N;
     rep(i,0,push_cnt) {
       ll nx = next_num(ind);
