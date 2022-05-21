612269e411d96e316c9d5953d0cbdcf0813d1368
Takahiro Hara <takahiro.hb@gmail.com>
2022-05-21 22:53:56 +0900

    add abc252 A-D

---
+++ abc252/c_slot_strategy.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+// #include <atcoder/segtree>
+using namespace std;
+// using namespace atcoder;
+typedef long long ll;
+typedef unsigned long long ull;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define fore(i,a) for(auto &i:a)
+// 桁数指定： cout << fixed << setprecision(10)
+
+double PI = 3.14159265359;
+
+// 逆元を求める関数
+long long modinv(long long a, long long m) {
+    long long b = m, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+
+ll MOD = 1000000007LL;
+vector<long long> fact_inv, inv, Com;
+
+/*  init_nCk :二項係数のための前処理
+    計算量:O(n)
+*/
+void init_nCk(int n, int SIZE) {
+    fact_inv.resize(SIZE + 5);
+    inv.resize(SIZE + 5);
+    fact_inv[0] = fact_inv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < SIZE + 5; i++) {
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+        fact_inv[i] = fact_inv[i - 1] * inv[i] % MOD;
+    }
+    Com.resize(SIZE + 5);
+    Com[0] = 1;
+    for (int i = 1; i < SIZE + 5; i++) {
+        Com[i] = Com[i - 1] * ((n - i + 1) * inv[i] % MOD) % MOD;
+    }
+}
+
+/*  nCk :MODでの二項係数を求める(前処理 int_nCk が必要)
+    計算量:O(1)
+*/
+long long nCk(int k) {
+    assert(!(k < 0));
+    return Com[k];
+}
+
+// UnionFind
+struct UnionFind {
+  vector<ll> par, siz;
+  
+  UnionFind(ll n) : par(n,-1), siz(n,1) {}
+  
+  ll root(ll x) {
+    if (par.at(x) == -1) return x;
+    else return par.at(x) = root(par.at(x));
+  }
+  
+  bool issame(ll x, ll y) {
+    return root(x) == root(y);
+  }
+  
+  bool unite(ll x, ll y) {
+    x = root(x); y = root(y);
+    
+    if (x == y) return false;
+    
+    if (siz.at(x) < siz.at(y)) swap(x, y);
+    
+    par.at(y) = x;
+    siz.at(x) += siz.at(y);
+    return true;
+  }
+  
+  ll size(ll x) {
+    return siz.at(root(x));
+  }
+};
+
+// 累乗（の剰余）
+long long modpow(long long a, long long n, long long mod) {
+    long long res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+int INF = 1000000000;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<string> S(N);
+  rep(i,0,N) cin >> S.at(i);
+  
+  int res = INF;
+  
+  rep(i,0,10) {
+    vector<int> cnts(10);
+    
+    rep(j,0,N) {
+      string str = S.at(j);
+      rep(k,0,10) {
+        int num = str.at(k) - '0';
+        if (num == i) cnts.at(k)++;
+      }
+    }
+    
+    int max_cnt = 0;
+    rep(l,0,10) max_cnt = max(max_cnt, cnts.at(l));
+    int idx = -1;
+    for (int m=9; m>=0; m--) {
+      if (cnts.at(m) == max_cnt) {
+        idx = m;
+        break;
+      }
+    }
+    
+    res = min(res, idx + 10 * (cnts.at(idx) - 1));
+  }
+  
+  cout << res << endl;
+}
+
