29e36ae5f794ad1bc90a271f0ec4c432e7f285c2
Takahiro Hara <takahiro.hb@gmail.com>
2022-05-21 15:07:14 +0900

    udpate lib

--- lib/lib.cpp
+++ lib/lib.cpp
@@ -10,6 +10,19 @@ typedef unsigned long long ull;
 
 double PI = 3.14159265359;
 
+// 逆元を求める関数
+long long modinv(long long a, long long m) {
+    long long b = m, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+
 ll MOD = 1000000007LL;
 vector<long long> fact_inv, inv, Com;
 
@@ -72,7 +85,7 @@ struct UnionFind {
   }
 };
 
-// 累乗
+// 累乗（の剰余）
 long long modpow(long long a, long long n, long long mod) {
     long long res = 1;
     while (n > 0) {
