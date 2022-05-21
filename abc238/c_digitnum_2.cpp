19260dfee8a9f0529b30be76f2186183721c4a67
Takahiro Hara <takahiro.hb@gmail.com>
2022-05-21 20:43:40 +0900

    fix abc238 C2

--- abc238/c_digitnum_2.cpp
+++ abc238/c_digitnum_2.cpp
@@ -1,18 +1,14 @@
 // 逆元を使って解く方法
 #include <bits/stdc++.h>
-#include <atcoder/segtree>
 using namespace std;
-using namespace atcoder;
 typedef long long ll;
 typedef unsigned long long ull;
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define fore(i,a) for(auto &i:a)
-// 桁数指定： cout << fixed << setprecision(10)
 
-double PI = 3.14159265359;
+ll MOD = 998244353LL;
 
-// 逆元を求める関数
-long long modinv(long long a, long long m) {
+ll modinv(long long a, long long m) {
     long long b = m, u = 1, v = 0;
     while (b) {
         long long t = a / b;
@@ -24,80 +20,38 @@ long long modinv(long long a, long long m) {
     return u;
 }
 
-ll MOD = 1000000007LL;
-vector<long long> fact_inv, inv, Com;
-
-/*  init_nCk :二項係数のための前処理
-    計算量:O(n)
-*/
-void init_nCk(int n, int SIZE) {
-    fact_inv.resize(SIZE + 5);
-    inv.resize(SIZE + 5);
-    fact_inv[0] = fact_inv[1] = 1;
-    inv[1] = 1;
-    for (int i = 2; i < SIZE + 5; i++) {
-        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
-        fact_inv[i] = fact_inv[i - 1] * inv[i] % MOD;
-    }
-    Com.resize(SIZE + 5);
-    Com[0] = 1;
-    for (int i = 1; i < SIZE + 5; i++) {
-        Com[i] = Com[i - 1] * ((n - i + 1) * inv[i] % MOD) % MOD;
-    }
-}
-
-/*  nCk :MODでの二項係数を求める(前処理 int_nCk が必要)
-    計算量:O(1)
-*/
-long long nCk(int k) {
-    assert(!(k < 0));
-    return Com[k];
-}
-
-// UnionFind
-struct UnionFind {
-  vector<ll> par, siz;
-  
-  UnionFind(ll n) : par(n,-1), siz(n,1) {}
-  
-  ll root(ll x) {
-    if (par.at(x) == -1) return x;
-    else return par.at(x) = root(par.at(x));
-  }
-  
-  bool issame(ll x, ll y) {
-    return root(x) == root(y);
-  }
-  
-  bool unite(ll x, ll y) {
-    x = root(x); y = root(y);
-    
-    if (x == y) return false;
-    
-    if (siz.at(x) < siz.at(y)) swap(x, y);
-    
-    par.at(y) = x;
-    siz.at(x) += siz.at(y);
-    return true;
-  }
-  
-  ll size(ll x) {
-    return siz.at(root(x));
-  }
-};
-
-// 累乗（の剰余）
-long long modpow(long long a, long long n, long long mod) {
-    long long res = 1;
-    while (n > 0) {
-        if (n & 1) res = res * a % mod;
-        a = a * a % mod;
-        n >>= 1;
-    }
-    return res;
+ll calc_sum(ll num) {
+  return (num % MOD) * ((num+1) % MOD) % MOD * modinv(2,MOD) % MOD;
 }
 
 int main() {
+  ll N;
+  cin>>N;
   
+  string S = to_string(N);
+  int siz = S.size()-1;
+  
+  ll sum = 0;
+  rep(i,0,siz) {
+    ll kake = 9;
+    rep(j,0,i) {
+      kake *= 10;
+    }
+    
+    sum += calc_sum(kake);
+    sum %= MOD;
+  }
+  
+  ll minus = 1;
+  ll tmpN = N;
+  while (tmpN > 0) {
+    tmpN /= 10;
+    if (tmpN == 0) break;
+    minus *= 10;
+  }
+  minus -= 1;
+  
+  sum += calc_sum(N - minus);
+  sum %= MOD;
+  cout << sum << endl;
 }
-
