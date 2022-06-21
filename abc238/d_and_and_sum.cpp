4b21de2135105f93effb886d75f1e4e8d8d3609a
原昂大 <takahiro.hara@ietty.co.jp>
2022-06-21 15:27:42 +0900

    add abc238 D

---
+++ abc238/d_and_and_sum.cpp
@@ -0,0 +1,55 @@
+// s-2aが2進数で表したaの0となる桁をiとした時、s-2aの値が2^iの選択的な和に
+// なっていれば良いと考えて解いてACした。
+// 解説を見るとこんなことはしなくてももっと簡単な方法がある模様。
+#include <bits/stdc++.h>
+// #include <atcoder/segtree>
+using namespace std;
+// using namespace atcoder;
+typedef long long ll;
+typedef unsigned long long ull;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define fore(i,a) for(auto &i:a)
+// 桁数指定： cout << fixed << setprecision(10)
+
+int main() {
+  int T;
+  cin >> T;
+  
+  vector<ull> rui2(60);
+  rep(i,0,60) {
+    ull tmp = 1;
+    rep(j,0,i) {
+      tmp *= 2;
+    }
+    rui2.at(i) = tmp;
+  }
+  
+  rep(ite,0,T) {
+    ull a,s;
+    cin >> a >> s;
+    
+    if (s < 2*a) {
+      cout << "No" << endl;
+      continue;
+    }
+    
+    ull tmp = s - 2*a;
+    
+    vector<ull> keta(60);
+    ull cnt = 0;
+    while (a > 0) {
+      if (a % 2 != 0) keta.at(cnt) = 1;
+      a /= 2;
+      cnt++;
+    }
+    
+    for (int i=59; i>=0; i--) {
+      if (keta.at(i) == 0 && tmp > 0 && tmp >= rui2.at(i)) {
+        tmp -= rui2.at(i);
+      }
+    }
+    if (tmp == 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+  }
+}
+
