052b745c7e20e0504a7f07497d885e2aa6912812
Takahiro Hara <takahiro.hb@gmail.com>
2022-10-17 20:51:31 +0900

    update tessoku a10

--- tessoku/a10_resort_hotel.cpp
+++ tessoku/a10_resort_hotel.cpp
@@ -8,6 +8,14 @@ typedef unsigned long long ull;
 #define fore(i,a) for(auto &i:a)
 // 桁数指定： cout << fixed << setprecision(10)
 
+template<class T> bool chmax(T& a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  else return false;
+}
+
 int main() {
   int N; cin >> N;
   vector<int> A(N);
@@ -26,20 +34,12 @@ int main() {
   vector<int> max_front(N), max_back(N);
   int tmp_mf = -1, tmp_mb = -1;
   rep(i,0,N) {
-    if (tmp_mf < A.at(i)) {
-      max_front.at(i) = max(tmp_mf, A.at(i));
-      tmp_mf = A.at(i);
-    } else {
-      max_front.at(i) = tmp_mf;
-    }
+    if (chmax(tmp_mf, A.at(i))) max_front.at(i) = max(tmp_mf, A.at(i));
+    else max_front.at(i) = tmp_mf;
   }
   rep(i,0,N) {
-    if (tmp_mb < A.at(N-i-1)) {
-      max_back.at(N-i-1) = max(tmp_mb, A.at(N-i-1));
-      tmp_mb = A.at(N-i-1);
-    } else {
-      max_back.at(N-i-1) = tmp_mb;
-    }
+    if (chmax(tmp_mb, A.at(N-i-1))) max_back.at(N-i-1) = max(tmp_mb, A.at(N-i-1));
+    else max_back.at(N-i-1) = tmp_mb;
   }
 
   rep(i,0,D) {
