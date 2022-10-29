b64509b1cb05a5d0a224dc2d712f59edb56cb3ba
Takahiro Hara <takahiro.hb@gmail.com>
2022-10-29 23:18:14 +0900

    update abc275 C

--- abc275/c_counting_squares.cpp
+++ abc275/c_counting_squares.cpp
@@ -22,7 +22,6 @@ int main() {
         rep(c2,0,9) {
           if ((r1 == r2) && (c1 == c2)) continue;
           if (S.at(r2).at(c2) == '.') continue;
-          // cout << r1 << " " << c1 << " : " << r2 << " " << c2 << endl;
           int rdiff = r1 - r2;
           int cdiff = c1 - c2;
           int nr1 = r1 + cdiff;
@@ -33,7 +32,6 @@ int main() {
             if (S.at(nr1).at(nc1) == '#' && S.at(nr2).at(nc2) == '#') {
               vector<pair<int,int>> v1 = {make_pair(r1,c1), make_pair(r2,c2), make_pair(nr1,nc1), make_pair(nr2,nc2)};
               sort(v1.begin(), v1.end());
-              
               st.insert(v1);
             }
           }
@@ -45,22 +43,14 @@ int main() {
           if (nr3 >= 0 && nr3 < 9 && nr4 >= 0 && nr4 < 9 && nc3 >= 0 && nc3 < 9 && nc4 >= 0 && nc4 < 9) {
             if (S.at(nr3).at(nc3) == '#' && S.at(nr4).at(nc4) == '#') {
               vector<pair<int,int>> v2 = {make_pair(r1,c1), make_pair(r2,c2), make_pair(nr3,nc3), make_pair(nr4,nc4)};
-            sort(v2.begin(), v2.end());
-            st.insert(v2);
+              sort(v2.begin(), v2.end());
+              st.insert(v2);
             }
-            
           }
         }
       }
     }
   }
-
-  // for (auto v : st) {
-  //   rep(i,0,4) {
-  //     cout << v.at(i).first << " " << v.at(i).second << " : ";
-  //   }
-  //   cout << endl;
-  // }
 
   cout << st.size() << endl;
 }
