35c49e998368f2751bd303a6c2d540e624e5d827
Takahiro Hara <takahiro.hb@gmail.com>
2022-10-29 23:33:11 +0900

    refactor abcc275 C (after contest)

--- abc275/c_counting_squares.cpp
+++ abc275/c_counting_squares.cpp
@@ -8,12 +8,26 @@ typedef unsigned long long ull;
 #define fore(i,a) for(auto &i:a)
 // 桁数指定： cout << fixed << setprecision(10)
 
+vector<string> S(9);
+set<vector<pair<int,int>>> st;
+
+void add_pair(pair<int,int> p1, pair<int,int> p2, int rdiff, int cdiff) {
+  int nr1 = p1.first + rdiff;
+  int nc1 = p1.second + cdiff;
+  int nr2 = p2.first + rdiff;
+  int nc2 = p2.second + cdiff;
+  if (nr1 >= 0 && nr1 < 9 && nr2 >= 0 && nr2 < 9 && nc1 >= 0 && nc1 < 9 && nc2 >= 0 && nc2 < 9) {
+    if (S.at(nr1).at(nc1) == '#' && S.at(nr2).at(nc2) == '#') {
+      vector<pair<int,int>> v = {make_pair(p1.first,p1.second), make_pair(p2.first,p2.second), make_pair(nr1,nc1), make_pair(nr2,nc2)};
+      sort(v.begin(), v.end());
+      st.insert(v);
+    }
+  }
+}
+
 int main() {
-  vector<string> S(9);
   rep(i,0,9) cin >> S.at(i);
 
-  int ans = 0;
-  set<vector<pair<int,int>>> st;
   rep(r1,0,9) {
     rep(c1,0,9) {
       if (S.at(r1).at(c1) == '.') continue;
@@ -24,29 +38,8 @@ int main() {
           if (S.at(r2).at(c2) == '.') continue;
           int rdiff = r1 - r2;
           int cdiff = c1 - c2;
-          int nr1 = r1 + cdiff;
-          int nc1 = c1 - rdiff;
-          int nr2 = r2 + cdiff;
-          int nc2 = c2 - rdiff;
-          if (nr1 >= 0 && nr1 < 9 && nr2 >= 0 && nr2 < 9 && nc1 >= 0 && nc1 < 9 && nc2 >= 0 && nc2 < 9) {
-            if (S.at(nr1).at(nc1) == '#' && S.at(nr2).at(nc2) == '#') {
-              vector<pair<int,int>> v1 = {make_pair(r1,c1), make_pair(r2,c2), make_pair(nr1,nc1), make_pair(nr2,nc2)};
-              sort(v1.begin(), v1.end());
-              st.insert(v1);
-            }
-          }
-
-          int nr3 = r1 - cdiff;
-          int nc3 = c1 + rdiff;
-          int nr4 = r2 - cdiff;
-          int nc4 = c2 + rdiff;
-          if (nr3 >= 0 && nr3 < 9 && nr4 >= 0 && nr4 < 9 && nc3 >= 0 && nc3 < 9 && nc4 >= 0 && nc4 < 9) {
-            if (S.at(nr3).at(nc3) == '#' && S.at(nr4).at(nc4) == '#') {
-              vector<pair<int,int>> v2 = {make_pair(r1,c1), make_pair(r2,c2), make_pair(nr3,nc3), make_pair(nr4,nc4)};
-              sort(v2.begin(), v2.end());
-              st.insert(v2);
-            }
-          }
+          add_pair(make_pair(r1,c1), make_pair(r2,c2), cdiff, -rdiff);
+          add_pair(make_pair(r1,c1), make_pair(r2,c2), -cdiff, rdiff);
         }
       }
     }
