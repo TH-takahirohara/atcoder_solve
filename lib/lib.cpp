ab51653607b8efc20488dd52a738b0f18ad8d622
Takahiro Hara <takahiro.hb@gmail.com>
2022-10-30 15:54:40 +0900

    update lib

--- lib/lib.cpp
+++ lib/lib.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
-// #include <atcoder/segtree>
+#include <atcoder/all>
 using namespace std;
-// using namespace atcoder;
+using namespace atcoder;
 typedef long long ll;
 typedef unsigned long long ull;
 #define rep(i,a,b) for(int i=a;i<b;i++)
