2af28ad720883c7276d297fd709b3ddf05e19f8b
Takahiro Hara <takahiro.hb@gmail.com>
2022-12-06 02:56:24 +0900

    add tessoku A15

---
+++ tessoku/a15_compression.cpp
@@ -0,0 +1,27 @@
+// 座標圧縮
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef unsigned long long ull;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define fore(i,a) for(auto &i:a)
+// 桁数指定： cout << fixed << setprecision(10)
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  rep(i,0,N) cin >> A.at(i);
+
+  vector<int> SA = A;
+  sort(SA.begin(), SA.end());
+  SA.erase(unique(SA.begin(), SA.end()), SA.end());
+
+  vector<int> ans(N);
+  rep(i,0,N) {
+    ans.at(i) = lower_bound(SA.begin(), SA.end(), A.at(i)) - SA.begin() + 1;
+  }
+  rep(i,0,N) cout << ans.at(i) << " ";
+}
