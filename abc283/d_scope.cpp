60672789490815eae1acb14a573457281e7a8b12
Takahiro Hara <takahiro.hb@gmail.com>
2022-12-25 00:12:12 +0900

    update abc283 D

--- abc283/d_scope.cpp
+++ abc283/d_scope.cpp
@@ -14,7 +14,6 @@ int main() {
 
   vector<pair<int,int>> vec;
   stack<int> st;
-  // map<char,vector<int>> mp;
   vector<vector<int>> vec3(30, vector<int>(300005));
   rep(i,0,S.size()) {
     char now = S.at(i);
@@ -26,33 +25,15 @@ int main() {
       vec.push_back(make_pair(p_n, i));
     } else { // 英小文字
       vec3.at((int)(now - 'a')).at(i)++;
-      // if (!mp.count(now)) {
-      //   mp[now] = {i};
-      // } else {
-      //   mp.at(now).push_back(i);
-      // }
     }
   }
 
-  // vector<vector<int>> vec3(30, vector<int>(300005));
   rep(i,0,27) {
     rep(j,1,300002) {
       vec3.at(i).at(j) += vec3.at(i).at(j-1);
     }
   }
-  // for (auto p : mp) {
-  //   auto key = p.first;
-  //   // cout << key << endl;
-  //   auto ve = p.second;
-  //   rep(i,0,ve.size()) {
-  //     vec3.at((int)(key - 'a')).at(ve.at(i))++;
-  //   }
-  //   rep(i,1,300002) {
-  //     vec3.at((int)(key - 'a')).at(i) += vec3.at((int)(key - 'a')).at(i-1);
-  //   }
-  // }
 
-  // set<char> stc; // 箱
   vector<int> vc(30);
   int idx = 0;
   string ans = "Yes";
@@ -76,14 +57,6 @@ int main() {
           vc.at(j) = 0;
         }
       }
-      // for (auto pa : mp) {
-      //   auto key = pa.first;
-      //   auto ve = pa.second;
-      //   int num = vec3.at((int)(key - 'a')).at(r) - vec3.at((int)(key - 'a')).at(l);
-      //   if (num > 0) { // ()内にkeyの文字がある場合、箱から削除する
-      //     vc.at(key - 'a') = 0;
-      //   }
-      // }
       idx++;
     }
   }
