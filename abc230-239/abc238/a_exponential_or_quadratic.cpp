dcd48d96daf84e89018243a8b97d51372664c441
Takahiro Hara <takahiro.hb@gmail.com>
2023-08-11 23:12:20 +0900

    add abc238 A

---
+++ abc230-239/abc238/a_exponential_or_quadratic.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <atcoder/segtree>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef unsigned long long ull;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define fore(i,a) for(auto &i:a)
+// 桁数指定： cout << fixed << setprecision(10)
+
+int main() {
+  int N;
+  cin >> N;
+
+  if (N == 1 || N >= 5) cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
