c4f3fed091a440072e49071aa5028997fc4e8800
Takahiro Hara <takahiro.hb@gmail.com>
2023-11-26 00:44:03 +0900

    update abc330 D

--- abc330/d_counting_ls.cpp
+++ abc330/d_counting_ls.cpp
@@ -14,23 +14,6 @@ int main() {
   vector<string> S(N);
   rep(i,0,N) cin >> S.at(i);
 
-  // vector<vector<int>> rui_yoko(N, vector<int>(N+1)), rui_tate(N, vector<int>(N+1));
-  // rep(i,0,N) {
-  //   rep(j,0,N) {
-  //     int a = 0;
-  //     if (S.at(i).at(j) == 'o') a++;
-  //     rui_yoko.at(i).at(j+1) = rui_yoko.at(i).at(j) + a;
-  //   }
-  // }
-
-  // rep(j,0,N) {
-  //   rep(i,0,N) {
-  //     int a = 0;
-  //     if (S.at(i).at(j) == 'o') a++;
-  //     rui_tate.at(j).at(i+1) = rui_tate.at(j).at(i) + a;
-  //   }
-  // }
-
   vector<ll> yoko(N), tate(N);
   rep(i,0,N) {
     ll tmp = 0;
@@ -51,10 +34,6 @@ int main() {
   rep(i,0,N) {
     rep(j,0,N) {
       if (S.at(i).at(j) != 'o') continue;
-      // ll yoko = rui_yoko.at(i).at(N) - rui_yoko.at(i).at(j);
-      // ll tate = rui_tate.at(j).at(N) - rui_tate.at(j).at(i);
-      // ll tmp = yoko * tate;
-      // ans += tmp;
       ll yo = yoko.at(i) - 1;
       ll ta = tate.at(j) - 1;
       ans += yo * ta;
